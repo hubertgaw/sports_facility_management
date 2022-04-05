447d768d9e233a750cec55055b1df273c9a724e9
Hubert Gawlowski <hubertgaw@gmail.com>
2022-04-05 19:53:52 +0200

    Create logging in

---
+++ src/main/java/pl/lodz/hubertgaw/resource/AuthenticationResource.java
@@ -0,0 +1,38 @@
+package pl.lodz.hubertgaw.resource;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.identity.SecurityIdentity;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import static java.util.Objects.requireNonNull;
+
+@Path("/login")
+@Authenticated
+public class AuthenticationResource {
+
+    private final Template page;
+
+    @Inject
+    SecurityIdentity identity;
+
+    public AuthenticationResource(Template page) {
+        this.page = requireNonNull(page, "page is required");
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get(@QueryParam("name") String name) {
+        return page
+                .data("name", name)
+                .data("identity", identity);
+
+    }
+}
